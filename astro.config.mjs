17bb58d976291bf8ae9787d5fc99325aef2e40b2
Fue <fher@atlassian.com>
2026-01-26 11:23:20 -0800

    favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import db from '@astrojs/db';
-import vercel from '@astrojs/vercel';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import db from "@astrojs/db";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: "server",
   adapter: vercel(),
-  integrations: [tailwind(), db()]
+  integrations: [tailwind(), db()],
 });
